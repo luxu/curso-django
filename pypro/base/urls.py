d08179bd1974d89597664d0d899968db1e409bda
Luciano Martins <zicadopv@gmail.com>
2021-09-05 11:17:50 -0300

    Update Pipfile - Pipfile.lock

--- pypro/base/urls.py
+++ pypro/base/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 
-from pypro.base.views import home, trader, investy
+from . import views
 
 app_name = 'base'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('trader/<str:country>/<str:stock>/', trader, name='trader'),
-    path('trader/', investy, name='investy'),
+    path('', views.home, name='home')
 ]
--- pypro/base/views.py
+++ pypro/base/views.py
@@ -1,5 +1,3 @@
-import investpy
-from django.http import JsonResponse
 from django.shortcuts import render
 
 
@@ -7,23 +5,3 @@ def home(request):
     template_name = 'base/home.html'
     context = {}
     return render(request, template_name, context)
-
-
-def trader(request, country, stock):
-    # template_name = 'base/home.html'
-    # context = {}
-    if not country:
-        country = 'Brazil'
-        stock = 'PETR4'
-    df = investpy.get_stock_historical_data(
-        stock=stock.upper(),
-        country=country,
-        from_date='01/01/2010',
-        to_date='01/01/2020'
-    )
-    print(df.head())
-    return JsonResponse(df.tail(), safe=False)
-
-
-def investy(request):
-    return render(request)
