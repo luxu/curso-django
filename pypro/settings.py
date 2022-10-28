46b90c4b48c6b6ef56d773cd17879eb283b3eb30
Luciano Martins <zicadopv@gmail.com>
2022-10-28 08:41:03 -0300

    Subir alterações

--- pypro/settings.py
+++ pypro/settings.py
@@ -10,6 +10,7 @@ import cloudinary.uploader
 import cloudinary.api
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+ROOT_DIR = Path(__file__).ancestor(2)
 
 SECRET_KEY = config('SECRET_KEY')
 
@@ -73,9 +74,7 @@ if DEBUG:
     )
 
 default_db_url = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
-
 DATABASES = {
     'default': config('DATABASE_URL', default=default_db_url, cast=parse_database)
 }
--- pypro/tests/test_home.py
+++ pypro/tests/test_home.py
@@ -16,7 +16,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Python Pro</title>')
+    assert_contains(resp, 'agora')
 
 
 def test_home_link(resp):
